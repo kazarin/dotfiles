92c9250062595716f94f5cbfb5dca5fa42ca9667
Konstantin S Kazarin <kazarin.konstantin@gmail.com>
2017-03-16 13:27:58 +1000

    Add awesome wm configs

---
+++ .config/awesome/disable_touchpad.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Off" 2
+
+
+
---
+++ .config/awesome/lang.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+setxkbmap -layout "us,ru" -option "grp:caps_toggle,grp_led:scroll"
